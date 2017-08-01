7d1d2cbc42947c999564442eb49ddd8b925cd241
Your Name <you@example.com>
2017-08-01 14:23:29 +0800

    resove minSdkVersion problem2

--- app/src/main/java/yxf/teachme/MainApplication.java
+++ app/src/main/java/yxf/teachme/MainApplication.java
@@ -1,6 +1,5 @@
 package yxf.teachme;
 
-import android.app.Application;
 import android.content.Context;
 import android.os.Handler;
 import android.os.StrictMode;
--- app/src/main/java/yxf/teachme/activity/SplashActivity.java
+++ app/src/main/java/yxf/teachme/activity/SplashActivity.java
@@ -7,11 +7,15 @@ import android.view.Window;
 import android.widget.TextView;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import rx.Observable;
 import rx.Observer;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Action1;
 import rx.schedulers.Schedulers;
 import yxf.teachme.R;
 import yxf.teachme.model.ZhuangbiImage;
@@ -29,17 +33,28 @@ public class SplashActivity extends BaseActivity {
         initView();
         initEvent();
     }
-    @OnClick(R.id.versionCode)
-    void clicktTest(){
-        Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
-        startActivity(intent);
-        finish();
-    }
+//    @OnClick(R.id.versionCode)
+//    void clicktTest(){
+//        Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+//        startActivity(intent);
+//        finish();
+//    }
     private void initView(){
         //todo:模拟一个网络请求获取versioncode并显示
         getVersionCode();
     }
     private void initEvent(){
+        // for test
+        Observable.timer(3*1000, TimeUnit.MILLISECONDS)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Action1<Long>() {
+                    @Override
+                    public void call(Long aLong) {
+                    Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+                    startActivity(intent);
+                    finish();
+                    }
+                });
 
     }
     private void getVersionCode(){
